f8515259f5447cb6235f8c60b9e2857acdb4c6f7
Madat Bay <medet.bayramov22@gmail.com>
2021-09-14 15:00:25 +0400

    bulma.css added

--- membership/views.py
+++ membership/views.py
@@ -2,4 +2,4 @@ from django.http.response import JsonResponse
 from django.shortcuts import render
 
 def index(request):
-    return JsonResponse({"message": "Index"})
+    return render(request, "membership/index.html")
